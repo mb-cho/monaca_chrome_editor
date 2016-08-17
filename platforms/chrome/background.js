5fc9503d31063e1b4ef9321e9417682a212a983e
mb-cho <aoki.m.f@gmail.com>
2016-08-17 21:48:16 +0900

    保存前

--- platforms/chrome/background.js
+++ platforms/chrome/background.js
@@ -4,7 +4,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('www/index.html', {
     bounds: {
-      width: 480,
+      width: 680,
       height: 640,
       left: 500,
       top: 200
